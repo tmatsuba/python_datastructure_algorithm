431571c31aab215a29899e93939b770daee60ae6
matsuba <matsuba@9105-109548.local>
2017-10-26 17:20:53 +0900

    anagramをbinary_searchを使うように修正

--- array_sequence/anagram.py
+++ array_sequence/anagram.py
@@ -1,7 +1,17 @@
+# --- coding: utf-8 ---
+
+from  array_sequence.binary_search import binary_search
 
 def anagram(s1,s2):
     sorted_s1 = sorted(s1.replace(" ", ""))
     sorted_s2 = sorted(s2.replace(" ", ""))
 
-    return sorted_s1 == sorted_s2
+    for src in sorted_s1:
+        index = binary_search(sorted_s2, src)
+        if index >= 0:
+            sorted_s2.pop(index)
+        else:
+            return False
+
+    return len(sorted_s2) == 0
 
--- array_sequence/array_pair_sum.py
+++ array_sequence/array_pair_sum.py
@@ -1,6 +1,5 @@
 # --- coding: utf-8 ---
 
-import math
 from  array_sequence.binary_search import binary_search
 
 
