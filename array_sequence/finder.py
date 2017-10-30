44fbad6e1aee66765b8990a7a761a20e0f50b4b2
tmatsuba <tma@gside.org>
2017-10-31 01:05:58 +0900

    finderの別の解法を実装

--- array_sequence/finder.py
+++ array_sequence/finder.py
@@ -18,3 +18,18 @@ def finder(arr1, arr2):
         else:
             sorted_arr2.pop(index)
 
+def finder2(arr1, arr2):
+    u"""arr1のうち、arr2に存在しない値を1つ表示する
+    """
+
+    if (len(arr1) - len(arr2)) != 1:
+        return
+
+    sorted_arr1 = sorted(arr1)
+    sorted_arr2 = sorted(arr2)
+
+    for num1, num2 in zip(sorted_arr1, sorted_arr2):
+        if num1 != num2:
+            return num1
+
+    return sorted_arr1[-1]
--- tests/test_finder.py
+++ tests/test_finder.py
@@ -1,7 +1,7 @@
 # --- coding: utf-8 ---
 
 from unittest import TestCase
-from  array_sequence.finder import finder
+from  array_sequence.finder import finder, finder2
 
 class TestAnagram(TestCase):
 
@@ -11,3 +11,10 @@ class TestAnagram(TestCase):
         self.assertEqual(finder([1,2,3,4,5,6,7],[3,7,2,1,4,6]),5)
 
         self.assertEqual(finder([1,2,3,4,5,6,7],[3,7,2,1,4,6, 1]), None)
+
+    def test_finder2(self):
+        self.assertEqual(finder2([5,5,7,7],[5,7,7]),5)
+        self.assertEqual(finder2([9,8,7,6,5,4,3,2,1],[9,8,7,5,4,3,2,1]),6)
+        self.assertEqual(finder2([1,2,3,4,5,6,7],[3,7,2,1,4,6]),5)
+
+        self.assertEqual(finder2([1,2,3,4,5,6,7],[3,7,2,1,4,6, 1]), None)
