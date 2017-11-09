9c0bebbe463910e1f8381cf7d2c8d8de87d224d2
tmatsuba <tma@gside.org>
2017-11-09 20:52:38 +0900

    sentence_reverceの別の解法を追加

--- array_sequence/sentence_reverce.py
+++ array_sequence/sentence_reverce.py
@@ -7,3 +7,32 @@ def sentence_reverce(str):
         return
 
     return ' '.join(reversed(str.split()))
+
+def sentence_reverce2(str):
+    u"""センテンスを逆順に並べ換えた文字列を返す(split, reversedを使わない)
+    """
+    if len(str.strip()) == 0:
+        return
+
+    length = len(str)
+    spaces = [' ', '	']
+    words = []
+    i = 0
+
+    while i < length:
+        if str[i] not in spaces:
+            word_start = i
+
+            while i < length and str[i] not in spaces:
+                i +=  1
+            words.append(str[word_start:i])
+        i += 1
+
+    rev_words = []
+    i = len(words)
+
+    while i > 0:
+        rev_words.append(words[i - 1])
+        i -= 1
+
+    return ' '.join(rev_words)
--- tests/test_sentence_reverce.py
+++ tests/test_sentence_reverce.py
@@ -1,14 +1,25 @@
 # --- coding: utf-8 ---
 
 from unittest import TestCase
-from  array_sequence.sentence_reverce import sentence_reverce
+from  array_sequence.sentence_reverce import sentence_reverce, sentence_reverce2
 
 class TestSentenceReverce(TestCase):
 
     def test_sentence_reverce(self):
         self.assertEqual(sentence_reverce('    space before'),'before space')
         self.assertEqual(sentence_reverce('space after     '),'after space')
+        self.assertEqual(sentence_reverce2('	space before'),'before space')
         self.assertEqual(sentence_reverce('   Hello John    how are you   '),'you are how John Hello')
         self.assertEqual(sentence_reverce('1'),'1')
         self.assertEqual(sentence_reverce(''),None)
         self.assertEqual(sentence_reverce('    '),None)
+
+
+    def test_sentence_reverce2(self):
+        self.assertEqual(sentence_reverce2('    space before'),'before space')
+        self.assertEqual(sentence_reverce2('	space before'),'before space')
+        self.assertEqual(sentence_reverce2('space after     '),'after space')
+        self.assertEqual(sentence_reverce2('   Hello John    how are you   '),'you are how John Hello')
+        self.assertEqual(sentence_reverce2('1'),'1')
+        self.assertEqual(sentence_reverce2(''),None)
+        self.assertEqual(sentence_reverce2('    '),None)
