1ca02efac3ed430962bf3a4c9df62bf894c6e15c
tmatsuba <tma@gside.org>
2018-06-24 09:23:21 +0900

    Pointerバージョンを追加

--- linked_list/nth_to_last_node.py
+++ linked_list/nth_to_last_node.py
@@ -14,7 +14,7 @@ def nth_to_last_node(n , head):
     cnt = 0
     lis = []
 
-    while cur is not None:
+    while cur:
         lis.append(cur)
         cur = cur.nextnode
 
@@ -22,3 +22,27 @@ def nth_to_last_node(n , head):
         raise Exception("index out bounds")
 
     return lis[len(lis) - n]
+
+def nth_to_last_node2(n , head):
+    u""" 与えられたLinked List の後ろからn番目を取得する。
+    """
+    if n <= 0:
+        raise Exception("n must not negative")
+
+    if isinstance(head, Node) == False:
+        raise Exception("arg must Node instance")
+
+    right_pointer = head
+    left_pointer = head
+
+    for i in range(n):
+        if not right_pointer.nextnode:
+            LookupError("index out bounds")
+
+        right_pointer = right_pointer.nextnode
+
+    while right_pointer:
+        right_pointer = right_pointer.nextnode
+        left_pointer = left_pointer.nextnode
+
+    return left_pointer
--- tests/test_nth_to_last_node.py
+++ tests/test_nth_to_last_node.py
@@ -2,7 +2,7 @@
 
 from unittest import TestCase
 from linked_list.sinele_linked_list import Node
-from linked_list.nth_to_last_node import nth_to_last_node
+from linked_list.nth_to_last_node import nth_to_last_node, nth_to_last_node2
 
 # Create a list of 4 nodes
 a = Node(1)
@@ -37,3 +37,19 @@ class TestNthToLastNode(TestCase):
             nth_to_last_node(-1, a)
             nth_to_last_node(1, None)
             nth_to_last_node(100, a)
+
+    def test_nth_to_last_node2(self):
+
+        self.assertEqual(a.nextnode.value, 2)
+        self.assertEqual(b.nextnode.value, 3)
+        self.assertEqual(c.nextnode.value, 4)
+
+        self.assertEqual(nth_to_last_node2(1, a), e)
+        self.assertEqual(nth_to_last_node2(2, a), d)
+        self.assertEqual(nth_to_last_node2(3, a), c)
+        self.assertEqual(nth_to_last_node2(4, a), b)
+
+        with self.assertRaises(Exception):
+            nth_to_last_node2(-1, a)
+            nth_to_last_node2(1, None)
+            nth_to_last_node2(100, a)
